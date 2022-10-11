30cf65e3890f5d150aa6e87986a07a82c50f8e5d
tarni-s-p <103916144+tarni-s-p@users.noreply.github.com>
2022-10-11 22:04:55 +0530

    Delete p2-4.py

--- p2-4.py
+++
@@ -1,8 +0,0 @@
-P=int(input("enter the value of P :"))
-R=int(input("enter the value of R :"))
-N=int(input("enter the value of N :"))
-T=int(input("enter the value of T :"))
-CI=P*(1+(R/(100*N)))**(N*T)
-print("COMPOUND INTEREST:")
-print(CI)
-Footer
